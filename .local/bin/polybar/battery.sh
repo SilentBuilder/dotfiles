c15aaabf9bec308f2a273bdd4b1d64750dd3cd48
SilentBuilder <newsilentbuilder@gmail.com>
2025-01-05 07:58:47 +0200

    QOL changes

--- .local/bin/polybar/battery.sh
+++ .local/bin/polybar/battery.sh
@@ -2,57 +2,57 @@
 
 # No battery detected
 if [ -z "$(shopt -s nullglob; echo /sys/class/power_supply/BAT?*)" ]; then
-    printf "󱟩\n"
+  printf "󱟩\n"
 fi
 
 for battery in /sys/class/power_supply/BAT?*; do
-    # If non-first battery, print a space separator.
-    [ -n "${capacity+x}" ] && printf " "
+  # If non-first battery, print a space separator.
+  [ -n "${capacity+x}" ] && printf " "
 
-    capacity="$(cat "$battery/capacity" 2>&1)"
-    case "$(cat "$battery/status" 2>&1)" in
-        "Full") status="󰁹 " ;;
-        "Discharging") 
-            case 1 in
-                $(( capacity >= 98 )) ) status="󰁹 " ;;
-                $(( capacity >= 90 )) ) status="󰂂 " ;;
-                $(( capacity >= 80 )) ) status="󰂁 " ;;
-                $(( capacity >= 70 )) ) status="󰂀 " ;;
-                $(( capacity >= 60 )) ) status="󰁿 " ;;
-                $(( capacity >= 50 )) ) status="󰁾 " ;;
-                $(( capacity >= 40 )) ) status="󰁽 " ;;
-                $(( capacity >= 30 )) ) status="󰁼 " ;;
-                $(( capacity >= 20 )) ) status="󰁻 " ;;
-                $(( capacity >= 10 )) ) status="󰁺 " ;;
-                *) status="󰂎 " ;;
-            esac
-        ;;
-        "Charging")
-            case 1 in
-                $(( capacity >= 98 )) ) status="󰂅 " ;;
-                $(( capacity >= 90 )) ) status="󰂋 " ;;
-                $(( capacity >= 80 )) ) status="󰂊 " ;;
-                $(( capacity >= 70 )) ) status="󰢞 " ;;
-                $(( capacity >= 60 )) ) status="󰂉 " ;;
-                $(( capacity >= 50 )) ) status="󰢝 " ;;
-                $(( capacity >= 40 )) ) status="󰂈 " ;;
-                $(( capacity >= 30 )) ) status="󰂇 " ;;
-                $(( capacity >= 20 )) ) status="󰂆 " ;;
-                $(( capacity >= 10 )) ) status="󰢜 " ;;
-                *) status="󰢟 " ;;
-            esac
-        ;;
-        "Not charging") status="󰂃 " ;;
-        "Unknown") status="󰂑 " ;;
-        *) exit 1 ;;
-    esac
+  capacity="$(cat "$battery/capacity" 2>&1)"
+  case "$(cat "$battery/status" 2>&1)" in
+    "Full") status="󰁹 " ;;
+    "Discharging") 
+      case 1 in
+        $(( capacity >= 98 )) ) status="󰁹 " ;;
+        $(( capacity >= 90 )) ) status="󰂂 " ;;
+        $(( capacity >= 80 )) ) status="󰂁 " ;;
+        $(( capacity >= 70 )) ) status="󰂀 " ;;
+        $(( capacity >= 60 )) ) status="󰁿 " ;;
+        $(( capacity >= 50 )) ) status="󰁾 " ;;
+        $(( capacity >= 40 )) ) status="󰁽 " ;;
+        $(( capacity >= 30 )) ) status="󰁼 " ;;
+        $(( capacity >= 20 )) ) status="󰁻 " ;;
+        $(( capacity >= 10 )) ) status="󰁺 " ;;
+        *) status="󰂎 " ;;
+      esac
+    ;;
+    "Charging")
+      case 1 in
+        $(( capacity >= 98 )) ) status="󰂅 " ;;
+        $(( capacity >= 90 )) ) status="󰂋 " ;;
+        $(( capacity >= 80 )) ) status="󰂊 " ;;
+        $(( capacity >= 70 )) ) status="󰢞 " ;;
+        $(( capacity >= 60 )) ) status="󰂉 " ;;
+        $(( capacity >= 50 )) ) status="󰢝 " ;;
+        $(( capacity >= 40 )) ) status="󰂈 " ;;
+        $(( capacity >= 30 )) ) status="󰂇 " ;;
+        $(( capacity >= 20 )) ) status="󰂆 " ;;
+        $(( capacity >= 10 )) ) status="󰢜 " ;;
+        *) status="󰢟 " ;;
+      esac
+    ;;
+    "Not charging") status="󰂃 " ;;
+    "Unknown") status="󰂑 " ;;
+    *) exit 1 ;;
+  esac
 
-    # Check if battery level is below 5%
-    if [ "$capacity" -lt 5 ]; then
-        echo "Battery level is critically low. Initiating shutdown..."
-        sudo shutdown -h now
-    fi
+  # Check if battery level is below 5%
+  if [ "$capacity" -lt 5 ]; then
+    echo "Battery level is critically low. Initiating shutdown..."
+    sudo shutdown -h now
+  fi
 
-    printf "%s%d%%\n" "$status" "$capacity"
+  printf "%s%d%%\n" "$status" "$capacity"
 done
 
--- .local/bin/reset-org.sh
+++ .local/bin/reset-org.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 
-base_dir="$HOME/.hidden/Notes/Struct"
+base_dir="$HOME/.hidden/Notes/Struct/Plans"
 files=("Morning.org" "Afternoon.org" "Evening.org" "Night.org")
 
 for file in "${files[@]}"; do
-    file_path="$base_dir/$file"
+  file_path="$base_dir/$file"
 
-    sed -i 's/\[X\]/[ ]/g' "$file_path"
-    sed -i 's/\[-\]/[ ]/g' "$file_path"
-    sed -i 's/\[\([0-9]\+\)\/\([0-9]\+\)\]/[0\/\2]/g' "$file_path"
-    sed -i 's/DONE/TODO/g' "$file_path"
+  sed -i 's/\[X\]/[ ]/g' "$file_path"
+  sed -i 's/\[-\]/[ ]/g' "$file_path"
+  sed -i 's/\[\([0-9]\+\)\/\([0-9]\+\)\]/[0\/\2]/g' "$file_path"
+  sed -i 's/DONE/TODO/g' "$file_path"
+  sed -i '/^CLOSED: /d' "$file_path"
 done
