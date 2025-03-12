65eef71c38a72378b816b44572e7d204f4518fda
SilentBuilder <newsilentbuilder@gmail.com>
2025-03-12 19:11:23 +0200

    QOL fixes

--- .local/bin/reset-org.sh
+++ .local/bin/reset-org.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
 base_dir="$HOME/.hidden/Notes/Struct/Plans"
-files=("Routine.org")
+files=("R.org" "R-0001.org" "R-0002.org" "R-0003.org" "R-0004.org" "R-0005.org" \
+       "R-0006.org" "R-0007.org" "R-0008.org" "R-0009.org" "R-0010.org" "R-0011.org" \
+       "R-0012.org" "R-0013.org" "R-0014.org" "R-0015.org" "R-0016.org" "R-0017.org")
 
 for file in "${files[@]}"; do
   file_path="$base_dir/$file"
