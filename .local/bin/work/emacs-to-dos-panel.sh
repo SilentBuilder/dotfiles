3bce4d9bd5b2428b9f36b78d2a55027214760be7
SilentBuilder <newsilentbuilder@gmail.com>
2025-05-06 15:59:11 +0300

    Fix

--- .local/bin/work/emacs-to-dos-panel.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-emacs --eval "(progn
-    (split-window-right)
-    (find-file \"~/.hidden/Notes/Index.org\")
-    (other-window 1)
-    (find-file \"~/.hidden/Notes/Academic.org\")
-    (other-window 1)
-    (my/open-daily-tracker-only))"
